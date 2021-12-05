5006b88072f8e2eb01e5aa0279958c91ad4523f6
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2021-12-05 15:44:10 +0900

    Create RepositorySearchResultViewController

--- VIPER_Sample/Modules/SearchResult/Router/RepositorySearchResultRouter.swift
+++ VIPER_Sample/Modules/SearchResult/Router/RepositorySearchResultRouter.swift
@@ -5,4 +5,20 @@
 //  Created by Taisei Sakamoto on 2021/11/29.
 //
 
-import Foundation
+import UIKit
+
+
+final class RepositorySearchResultRouter {
+    // 画面遷移のためにViewControllerが必要。initで受け取る
+    private unowned let viewController: UIViewController
+    
+    private init(viewController: UIViewController) {
+        self.viewController = viewController
+    }
+    
+    // DI
+    static func assembleModules() -> UIViewController {
+        let view = RepositorySearchResultRouter()
+        
+    }
+}
---
+++ VIPER_Sample/Modules/SearchResult/ViewControllers/RepositorySearchResultViewController.swift
@@ -0,0 +1,10 @@
+//
+//  RepositorySearchResultViewController.swift
+//  VIPER_Sample
+//
+//  Created by Taisei Sakamoto on 2021/12/05.
+//
+
+import UIKit
+
+final class 

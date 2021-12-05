e6dd8be38c330a9d80fa57fdf48aca7950b7db93
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2021-12-05 18:26:34 +0900

    Entityの作成

--- VIPER_Sample/Infra/GitHubClient/Entities/RepositoryEntity.swift
+++ VIPER_Sample/Infra/GitHubClient/Entities/RepositoryEntity.swift
@@ -7,3 +7,20 @@
 
 import Foundation
 
+struct RepositoryEntity: Decodable {
+    let id: Int
+    let name: String
+    let fullname: String
+    let htmlUrl: URL
+    let starCount: Int
+    let owner: UserEntity
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case fullname = "full_name"
+        case htmlUrl = "html_url"
+        case starCount = "stargazers_count"
+        case owner
+    }
+}
---
+++ VIPER_Sample/Infra/GitHubClient/Entities/UserEntity.swift
@@ -0,0 +1,13 @@
+//
+//  UserEntity.swift
+//  VIPER_Sample
+//
+//  Created by Taisei Sakamoto on 2021/12/05.
+//
+
+import Foundation
+
+struct UserEntity: Decodable {
+    let id: String
+    let login: String
+}

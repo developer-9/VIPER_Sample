2c982bd9fe9fc2f5b9bdee2dd4531c20df4eac67
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2021-12-08 00:34:53 +0900

    UITableViewCell+Extensionの実装

--- VIPER_Sample/Extensions/UITableViewCell+Extension.swift
+++ VIPER_Sample/Extensions/UITableViewCell+Extension.swift
@@ -10,3 +10,22 @@ import UIKit
 protocol Registarable where Self: UIView {
     static var reuseIdentifier: String { get }
 }
+
+extension Registarable {
+    static var reuseIdentifier: String {
+        return self.className
+    }
+}
+
+protocol ClassRegistrable: Registarable {}
+
+protocol NibRegistrable: Registarable {
+    static var nib: UINib { get }
+}
+
+extension NibRegistrable {
+    static var nib: UINib {
+        let nibName = String(describing: self)
+        return UINib(nibName: nibName, bundle: Bundle(for: self))
+    }
+}

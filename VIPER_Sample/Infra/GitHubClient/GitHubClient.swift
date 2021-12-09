e4679367386aadd4fef38bec921f6cabd479a726
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2021-12-10 08:39:54 +0900

    WIP

--- VIPER_Sample/Infra/GitHubClient/GitHubClient.swift
+++ VIPER_Sample/Infra/GitHubClient/GitHubClient.swift
@@ -8,5 +8,25 @@
 import Foundation
 
 protocol GitHubRequestable: AnyObject {
+    func send<Request: GitHubRequest>(request: Request, completion: @escaping(Result<Request.Response, Error>) -> ())
+}
+
+class GitHubClient {
     
+    private let session: URLSession
+    
+    init(session: URLSession = {
+        let configuration = URLSessionConfiguration.default
+        let session = URLSession(configuration: configuration)
+        return session
+    }()
+    ) {
+        self.session = session
+    }
+}
+
+extension GitHubClient: GitHubRequestable {
+    func send<Request>(request: Request, completion: @escaping (Result<Request.Response, Error>) -> ()) where Request : GitHubRequest {
+        <#code#>
+    }
 }

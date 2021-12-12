0e9ef507284e61448c38dc7f189e101e970e6e46
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2021-12-12 23:17:20 +0900

    WIP

--- VIPER_Sample/Domain/SearchRepository/SearchRepositoryInteractor.swift
+++ VIPER_Sample/Domain/SearchRepository/SearchRepositoryInteractor.swift
@@ -14,10 +14,15 @@ protocol SearchRepositoryUsecase: AnyObject {
 final class SearchRepositoryInteractor {
     // GitHubにアクセスするためのAPIクライアント
     // テスト時にAPIクライアントをMockに差し替えてレスポンスを返すようにする
+    private let client: GitHubRequestable
+    
+    init(client: GitHubRequestable = GitHubClient()) {
+        self.client = client
+    }
 }
 
 extension SearchRepositoryInteractor: SearchRepositoryUsecase {
     func fetchRepositories(keyword: String, completion: @escaping (Result<[RepositoryEntity], Error>) -> Void) {
-        <#code#>
+         
     }
 }

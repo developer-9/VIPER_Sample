a9c91151d87e3e9d2b2ac5085942f61d010e8a00
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2021-12-12 22:11:45 +0900

    Sendメソッドの実装

--- VIPER_Sample/Infra/GitHubClient/GitHubClient.swift
+++ VIPER_Sample/Infra/GitHubClient/GitHubClient.swift
@@ -26,7 +26,24 @@ class GitHubClient {
 }
 
 extension GitHubClient: GitHubRequestable {
-    func send<Request>(request: Request, completion: @escaping (Result<Request.Response, Error>) -> ()) where Request : GitHubRequest {
-        <#code#>
+    func send<Request: GitHubRequest>(request: Request, completion: @escaping (Result<Request.Response, Error>) -> ()) {
+        let urlRequest = request.buildURLRequest()
+        let task = session.dataTask(with: urlRequest) { data, response, error in
+            switch (data, response, error) {
+            case (_, _, let error?):
+                print(error.localizedDescription)
+                completion(.failure(error))
+            case (let data?, let response?, _):
+                do {
+                    let response = try request.response(from: data, urlResponse: response)
+                    completion(.success(response))
+                } catch {
+                    completion(.failure(NSError()))
+                }
+            default:
+                completion(.failure(NSError()))
+            }
+        }
+        task.resume()
     }
 }

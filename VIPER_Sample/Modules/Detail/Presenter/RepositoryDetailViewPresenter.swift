c2a4b784e973446317530b12fb90ccd5b670ba95
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2021-12-13 07:29:13 +0900

    DetailViewControllerとPresenterファイルの作成

---
+++ VIPER_Sample/Modules/Detail/Presenter/RepositoryDetailViewPresenter.swift
@@ -0,0 +1,12 @@
+//
+//  RepositoryDetailViewPresenter.swift
+//  VIPER_Sample
+//
+//  Created by Taisei Sakamoto on 2021/12/13.
+//
+
+import Foundation
+
+protocol RepositoryDetailViewPresentation: AnyObject {
+    
+}
---
+++ VIPER_Sample/Modules/Detail/Views/RepositoryDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RepositoryDetailViewController.swift
+//  VIPER_Sample
+//
+//  Created by Taisei Sakamoto on 2021/12/12.
+//
+
+import UIKit
+import WebKit
+
+protocol RepositoryDetailView: AnyObject {
+    
+}
+
+final class RepositoryDetailViewController: UIViewController {
+    
+    var presenter: RepositoryDetailViewPresentation!
+}

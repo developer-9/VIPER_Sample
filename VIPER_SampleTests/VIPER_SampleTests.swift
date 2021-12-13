7f76819c2ce6cc9753837dd03a10e212001c386d
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2021-12-13 23:14:39 +0900

    テストコードMockの追加

--- VIPER_SampleTests/VIPER_SampleTests.swift
+++ VIPER_SampleTests/VIPER_SampleTests.swift
@@ -9,18 +9,21 @@ import XCTest
 @testable import VIPER_Sample
 
 class VIPER_SampleTests: XCTestCase {
+    
+    var view: ViewMock!
+    var router: RouterMock!
+    var searchRepositoryInteractor: RepositoryInteractorMock!
+    var presenter: RepositorySearchResultPresenter!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        view = .init()
+        router = .init()
+        searchRepositoryInteractor = .init()
+        presenter = .init(view: view, router: router, searchRepositoryInteractor: searchRepositoryInteractor)
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
     func testPerformanceExample() throws {
@@ -30,19 +33,53 @@ class VIPER_SampleTests: XCTestCase {
         }
     }
     
+    
+    
     // MARK: - Mock
+    
     class ViewMock: RepositorySearchResultView {
+        
         var callCount_setLastSearchText = 0
         func setLastSearchText(_ text: String) {
             callCount_setLastSearchText += 1
         }
         
-        func updateRepositories(_ repositories: [RepositoryEntity]) {
-            <#code#>
+        var callCount_showRefreshView = 0
+        func showRefreshView() {
+            callCount_showRefreshView += 1
         }
         
+        var callCount_updateRepositories = 0
+        func updateRepositories(_ repositories: [RepositoryEntity]) {
+            callCount_updateRepositories += 1
+        }
+        
+        var callCount_showErrorAlert = 0
         func showErrorAlert() {
-            <#code#>
+            callCount_showErrorAlert += 1
+        }
+    }
+    
+    class RouterMock:RepositorySearchResultWireframe {
+        
+        var isCalled_showRepositoryDetail = false
+        func showRepositoryDetail(_ repository: RepositoryEntity) {
+            isCalled_showRepositoryDetail = true
+        }
+    }
+    
+    class RepositoryInteractorMock: SearchRepositoryUsecase {
+        
+        let result: Result<[RepositoryEntity], Error>
+        
+        init(result: Result<[RepositoryEntity], Error> = .failure(NSError())) {
+            self.result = result
+        }
+        
+        var callCount_fetchRepositories = 0
+        func fetchRepositories(keyword: String, completion: @escaping (Result<[RepositoryEntity], Error>) -> Void) {
+            callCount_fetchRepositories += 1
+            completion(result)
         }
     }
 }
